2e080fd53a4c5ef4b249f39989300ef9bdfa963f
“enioaiello” <aielloenio@icloud.com>
2023-03-05 18:05:26 +0100

    Mise à jour du site

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let announcementPanel = document.querySelector("#openAnnouncement");
-let closeAnnouncementButton = document.querySelector("#announcementClose");
+let announcementPanel = document.querySelector(".announcement");
+let closeAnnouncementButton = document.querySelector(".announcementClose");
 
 function closeAnnounce() {
     announcementPanel.style.display = "none";
 }
 
 closeAnnouncementButton.addEventListener("click", closeAnnounce);
+
+let ip = document.querySelector("#ip");
+let copyClipboard = document.querySelector(".copyClipboard");
+
+function copyIP() {
+    navigator.clipboard.writeText("axtrale.mine.fun").then(() => {
+        copyClipboard.classList.remove("none");
+        setTimeout(() => {
+            copyClipboard.classList.add("none");
+          }, "5000");
+    }).catch((error) => {
+        console.error("L'IP n'a pas pu être copiée…");
+    });
+}
+
+ip.addEventListener("click", copyIP);
